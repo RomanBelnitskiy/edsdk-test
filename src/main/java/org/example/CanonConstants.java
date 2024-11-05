f08da48c0b6346e20731cd2871ba9396e0cf6b9c
RomanBelnitskiy <133407185+RomanBelnitskiy@users.noreply.github.com>
2024-11-05 12:41:00 +0200

    first commit

---
+++ src/main/java/org/example/CanonConstants.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class CannonConstants {
+    public static final int kEdsCameraCommand_TakePicture = 0x00000000;  // Replace with the correct command value from SDK docs.
+}
